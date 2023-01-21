9e91e8d280b3dbdc6a6e6c97fa79e943c5b17644
mzdak <35873406+mzdak@users.noreply.github.com>
2023-01-21 18:56:57 +0530

    add button to select grid size

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 let container = document.querySelector('.container');
-let resetButton = document.querySelector('.reset')
+let resetButton = document.querySelector('.reset');
+let gridSelect = document.querySelector('.grid-select');
 
 let gridSize = 16;
 let grid = [];
-for(i=0; i<(gridSize*gridSize); i++){
-    let box = document.createElement('div');
-    grid.push(box);
+
+function clearCanvas(){
+    grid.forEach(box => {
+        box.style.backgroundColor = 'white';
+    })
 }
 
-console.log(grid)
+function makeGrid(){
+    clearCanvas()
+    
+    for(i=0; i<(gridSize*gridSize); i++){
+        let box = document.createElement('div');
+        grid.push(box);
+    }
+    
+    console.log(grid)
+    
+    grid.forEach(box => {
+        box.style.minWidth = (700/gridSize).toString()+'px';
+        box.style.minHeight = (700/gridSize).toString()+'px';
+        container.appendChild(box);
 
-grid.forEach(box => {
-    // var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-    box.style.minWidth = (700/16).toString()+'px';
-    // box.style.backgroundColor = randomColor;
-    container.appendChild(box);
-});
+        box.addEventListener("mouseenter", function(){
+            box.style.backgroundColor = 'black';
+        })
+    });
+}
 
-grid.forEach(box => {
-    box.addEventListener("mouseenter", function(){
-        box.style.backgroundColor = 'black';
-    })
+resetButton.addEventListener('click', clearCanvas)
+
+gridSelect.addEventListener('click', function(){
+    select = Number(prompt("Enter a Grid size (1-100): ", '16'));
+    if(select<1 || select>100 || select == '' || select == null) alert('Invalid input');
+    else gridSize = select;
+    makeGrid();
 })
 
-resetButton.addEventListener('click', () => location.reload())
+makeGrid(); //Default grid (16X16)
+
+
+
+//For reference, the following will return a random colour:
+// var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
